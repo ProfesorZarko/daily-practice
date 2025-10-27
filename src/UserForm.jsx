5f2aea19e41cc7d8b0d3688f91cda7b79d949b50
ProfesorZarko <profesorfz2@gmail.com>
2025-10-27 08:02:50 +0100

    Day 4:Added users list

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -6,20 +6,37 @@ export default function UserForm() {
         firstName: "",
         age: "",
     })
+    
+    const [users, setUsers] = React.useState([
+        { id: 1, firstName: "Alice", age: 25 },
+    ])
 
     function handleSubmit(event) {
-        event.preventDefault()
+            event.preventDefault()
+         if (!formData.firstName.trim() || !formData.age.trim()) {
+        alert("Please fill in both fields!");
+        return;
+        }
+
+        setUsers([...users, {
+            id: users.length + 1,
+            firstName: formData.firstName,
+            age: formData.age,
+        }]);
         console.log("Form submitted:", formData)
+        setFormData({ firstName: "", age: "" });
+
     }
   return (
     <div className='userForm'>UserForm
-        <form onsubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="firstName">First Name</label>
             <input 
                 type="text" 
                 placeholder="First Name" 
                 value={formData.firstName}
                 onChange={(e) => setFormData({...formData, firstName: e.target.value})}
+                required
             />  
             <br />
             <label htmlFor="age">Age</label>
@@ -28,11 +45,15 @@ export default function UserForm() {
                 placeholder="Age" 
                 value={formData.age}
                 onChange={(e) => setFormData({...formData, age: e.target.value})}
+                required
             />
             <br />
             <button type="submit">Submit</button>
         </form>
         <h3>name : {formData.firstName} - age : {formData.age}</h3>
-    </div>
+        {users.map(user => (<div key={user.id}>
+            <h4>{user.firstName} - {user.age}</h4>
+        </div>))}
+    </div>  
   )
 }
