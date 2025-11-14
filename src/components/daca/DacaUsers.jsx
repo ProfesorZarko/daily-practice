99f7e520a9fb480a2dedd134c1d1ca8847d8edd4
ProfesorZarko <profesorfz2@gmail.com>
2025-11-14 09:57:54 +0100

    Day 9: Dodata stilizacija on hoover

--- src/components/daca/DacaUsers.jsx
+++ src/components/daca/DacaUsers.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React , {useState} from 'react'
+import './DacaUsers.css';
 
 export default function DacaUsers({users, onRemove: removeUser}) {
+    const [activeUser, setActiveUser] = useState(null);
+
   return (
     <div>
       <h3>Lista korisnika ({users.length})</h3>
       <ul>
         {users.map((u, i) => (
-          <li key={i}>
+          <li key={i}
+            className={activeUser === i ? "user active" : "user"}
+                onMouseEnter={() => setActiveUser(i)}
+                onMouseLeave={() => setActiveUser(null)}>
             {u.firstName} ({u.age}){" "}
             <button onClick={() => removeUser(i)}>X</button>
           </li>
